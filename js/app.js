187df5f98e573f0c9ec319664753d07c9a99cbe6
Sri <srisreed@gmail.com>
2013-07-05 14:15:50 -0700

    new question, updated js, updated css

--- js/app.js
+++ js/app.js
@@ -9,44 +9,49 @@ $(function(){
         console.log('App initialized!' + app);
         app.user_input = $('#search-field').val();
         app.user_search = $('#search-field');
-        app.submit = $('#submit-button');
+        app.submit = $('#submitBtn');
         app.game_form = $('#game-search form');
-
+        app.randomNum = 0;
+       
 
         app.game_form.on('submit', function(e){
             e.preventDefault();
-            app.find();
             $('#questions').html('');
-            $('#questions').append('<h1> Who is the main character of this game? </h1> <br>');
-            app.findData();
+            app.submit.removeClass('hidden');
+            app.find();
+            app.addQuestions();
         });
 
-        $(document).ajaxComplete(function(){
-            if($('.choices').length !== 0)
-                $('.choices').css('text-decoration', 'none');
-        });
+        //console.log($('#results'));
 
-        
-        console.log($('#results'));
+    }
 
-        // app.user_search.keypress(function(e){
-        //     if(e.which === 13) {
-        //         e.preventDefault();
-        //         app.find();
-        //     }
-        // });
-
-        // app.submit.on('click', function(e){
-        //     e.preventDefault();
-        //     console.log(app.submit);
-        //     app.find();
-        // });
-
-        // app.game_form.submit(function(){
-        //     app.find();
-        // });
-        //app.find();
-
+    /**
+     * Add question and choices
+     */
+    GBapp.prototype.addQuestions = function() {
+            $('#questions').append('<p> In which game did he/she make their first appearance?</p> <br>'); 
+            var q = ["Metal Gear Solid", "God Of War", "Super Smash Brothers", "Madden", "Ninja Gaiden"];
+                      
+            for(var i in q)
+            {
+                app.randomNum = Math.floor(q.length * Math.random());
+                //console.log(app.randomNum);
+                //console.log(q[app.randomNum]);
+                //console.log($("input:radio").val());
+                //console.log($("input:radio").val() !== q[app.randomNum]);
+                if($("input:radio").val() !== q[app.randomNum])
+                {
+                    $('#questions').append("<input type='radio' name='choice' value='" + q[app.randomNum] + "'/>" + q[app.randomNum] + "<br />");
+                }
+            }
+            //console.log($('#questions input:checked').val());
+            
+            
+            $('input:button').on('click', function(e) {
+                e.stopPropagation();
+                alert('hello');
+            });
     };
 
     /**
@@ -60,7 +65,7 @@ $(function(){
             data: {
                     api_key: app.api_key,
                     query: $('#search-field').val(),
-                    resources : "game",
+                    resources : "character",
                     format : "jsonp"
             },
             jsonp: 'json_callback',
@@ -69,15 +74,31 @@ $(function(){
             },
             success: function(data) {
                 $('#error').hide();
+                //app.submit.show();
                 console.log(data);
-                // var i;
+                var i;
                 // var datas = $('#results');
                 // datas.html('');
                 // for (i in data['results']) {
                 //     datas.append('<div>' + '<img src="' + data['results'][i]['image']['icon_url'] + '">' + ' is: <p>' + data['results'][i]['resource_type'] + '</p></div><br />');
                 // }
-                $('#results').append('<img src=' + data['results'][0].image.medium_url + '>');             
-                //$('#questions ul').append('<p>hello</p>');
+                for(i in data.results)
+                { 
+                    //console.log($('#search-field').val().toLowerCase());
+                    console.log(data['results'][i].name);
+                    
+                    if($('#search-field').val().toLowerCase() === data['results'][i].name.toLowerCase())
+                    {
+                         $('#results').append('<img src=' + data['results'][i].image.medium_url + '>');
+                         $('#questions').append("<input type='radio' name='choice' value='" + data['results'][i].first_appeared_in_game.name + "'/>" +data['results'][i].first_appeared_in_game.name + "<br />");
+                         break;
+                    }
+
+                    
+                    //$('#questions').append(data['results'][i].first_appeared_in_game.name);
+
+                }
+                //$('#questions').append('');
             },
             error: function() {
                 $('#error').show();
@@ -89,30 +110,65 @@ $(function(){
     };
 
     //&format=json&query=kratos&resources=character&field_list=name,first_appeared_in,image
-    GBapp.prototype.findData = function() {
+    GBapp.prototype.findGames = function() {
         $.ajax({
-            url: 'http://www.giantbomb.com/api/search',
+            url: 'http://www.giantbomb.com/api/games',
             dataType: 'jsonp',
             cache: false,
             data: {
                 api_key:  '7f4c7d2fa9de93b62e9d2cb05f83828d04119472',
-                query: "kratos",
-                resources: "character",
                 format: "jsonp",
-                field_list:"first_appeared_in,name,image"
+                field_list:"name,id",
+                offset: 0
             },
             jsonp: 'json_callback',
             beforeSend: function() {
                 $('#error').hide();
             },
             success: function(data) {
-                //$('#questions').append(data.results[0].name);
-                //$('#questions').append('<li  class="choices" style="clear:both;margin-bottom:0;text-decoration:none;" >Master</li>' +
-                //                        '<li class="choices" style="clear:both;margin-bottom:0;text-decoration:none;" >' + data.results[0].name + '</li>' +
-                //                        '<li class="choices" style="clear:both;margin-bottom:0;text-decoration:none;" >Balrog</li>');
+               // $('#questions').append(data.results[0].name);
+               //j+=100;
+               for(var i in data.results) {
+                    //if(data.results[i].name === $('#search-field').val())
+                    {
+                        //console.log(data.results[i].name + " | | " + $('#search-field').val());
+                        //console.log(data.results[i].name === $('#search-field').val());
+                        $('#questions').append('<li class="choices" style="list-style-type:none; display:inline-block" >' + data.results[i].name + '</li>' + ' // ');
+                        console.log(data.results[i]);
+                    }
+                }
             },
             error: function() {
-            
+                console.log('error');
+            }
+        });
+    };
+
+    GBapp.prototype.findCharacters = function() {
+        $.ajax({
+            url: 'http://www.giantbomb.com/api/characters',
+            dataType: 'jsonp',
+            cache: false,
+            data: {
+                api_key: '7f4c7d2fa9de93b62e9d2cb05f83828d04119472',
+                format: "jsonp",
+                field_list:"name,first_appeared_in_game",
+                offset: 0
+            },
+            jsonp: 'json_callback',
+            beforeSend: function() {
+
+            },
+            success: function(data) {
+                for(var i in data.results){
+                    console.log(" - - " + data.results[i].name !== null);   
+                    if(data.results[i].name !== null && data.results[i].first_appeared_in_game.name !== null) {
+                       console.log(data.results[i].name + " " + data.results[i].first_appeared_in_game.name);   
+                    }
+                }
+            },
+            error: function() {
+
             }
         });
     };
